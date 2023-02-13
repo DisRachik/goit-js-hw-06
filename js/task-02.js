403ced7517258771da21fe65a0ee7b43e7c7657c
DisRachik <radchenkoig@gmail.com>
2023-02-13 20:40:29 +0200

    the second task is completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const createListItems = items => {
+	return items.map(item => {
+		const liEl = document.createElement('li');
+		liEl.classList.add('item');
+		liEl.textContent = item;
+
+		return liEl;
+	});
+};
+
+const listElements = createListItems(ingredients);
+
+ingredientsListEl.append(...listElements);
