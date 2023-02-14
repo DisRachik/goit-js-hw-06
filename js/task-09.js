7555377aab94183b1cfe73c238777e775f5d9f41
DisRachik <radchenkoig@gmail.com>
2023-02-14 19:27:08 +0200

    the nineth task is completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const getRandomHexColor = () =>
+	`#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+const onChangeColorForBody = () => {
+	document.body.style.backgroundColor = getRandomHexColor();
+	spanEl.textContent = getRandomHexColor();
+};
+
+buttonEl.addEventListener('click', onChangeColorForBody);
