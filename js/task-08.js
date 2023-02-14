164e604e8416737895c18c2ca1a6ea0096031e62
DisRachik <radchenkoig@gmail.com>
2023-02-14 18:07:49 +0200

    the eighth task is completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+	event.preventDefault();
+	const { email, password } = event.currentTarget.elements;
+
+	if (!email.value || !password.value) {
+		alert('Please fill in all the fields!');
+	} else {
+		const formData = {
+			[email.name]: email.value,
+			[password.name]: password.value,
+		};
+		console.log(formData);
+
+		event.currentTarget.reset();
+	}
+};
+
+formEl.addEventListener('submit', onFormSubmit);
